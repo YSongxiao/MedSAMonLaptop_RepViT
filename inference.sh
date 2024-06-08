329649ab6232709648967b04a4b445045b4d4d89
Songxiao Yang <55771652+YSongxiao@users.noreply.github.com>
2024-06-08 17:22:20 +0900

    Delete inference.sh

--- inference.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 CVPR24_LiteMedSAM_infer_repvit.py -i validation/imgs/ -o validation/preds/ -device cpu -lite_medsam_ckpt_path path/to/teacher_model -encoder_ckpt_path path/to/distilled_encoder
