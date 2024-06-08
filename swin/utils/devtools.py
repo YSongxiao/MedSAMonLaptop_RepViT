842fb7cdeb25372af659e27d02c2dc500ea01e78
Songxiao Yang <55771652+YSongxiao@users.noreply.github.com>
2024-06-08 17:26:45 +0900

    Delete swin directory

--- swin/utils/devtools.py
+++
@@ -1,7 +0,0 @@
-
-def print_tensor_shape(inputs):
-    if isinstance(inputs,list) or isinstance(inputs,tuple):
-        for input in inputs:
-            print(input.shape)
-    else:
-        print(inputs)
